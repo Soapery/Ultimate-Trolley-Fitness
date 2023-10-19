c42140dc4c7853b3d767f286a29bdeae1ba93678
Soapery <darian.a.ronald@gmail.com>
2023-10-19 15:07:39 -0500

    Added isStepCounterAvailable and StepCounterListener

--- app/src/main/java/com/example/ultimatetrolleyfitness/StepCounterHelper.kt
+++ app/src/main/java/com/example/ultimatetrolleyfitness/StepCounterHelper.kt
@@ -1,2 +1,27 @@
 package com.example.ultimatetrolleyfitness
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+
+fun isStepCounterAvailable(context: Context): Boolean {
+    val sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    val stepCounterSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+    return stepCounterSensor != null
+}
+
+class StepCounterListener : SensorEventListener {
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        // Handle accuracy changes if needed
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event?.sensor?.type == Sensor.TYPE_STEP_COUNTER) {
+            val stepCount = event.values[0].toInt()
+            // Update your UI or data with the step count
+        }
+    }
+}
+
