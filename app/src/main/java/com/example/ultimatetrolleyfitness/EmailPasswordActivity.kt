c0e58cedaf565a05ea0f9406743a8b69e3406e8c
Soapery <darian.a.ronald@gmail.com>
2023-11-06 20:35:39 -0600

    Updated to utilize composable functions for display.

--- app/src/main/java/com/example/ultimatetrolleyfitness/EmailPasswordActivity.kt
+++ app/src/main/java/com/example/ultimatetrolleyfitness/EmailPasswordActivity.kt
@@ -2,172 +2,144 @@ package com.example.ultimatetrolleyfitness
 
 import android.content.Intent
 import android.os.Bundle
-import android.text.TextUtils
-import android.util.Log
-import android.util.Patterns
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.ultimatetrolleyfitness.databinding.ActivityEmailPasswordBinding
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.ultimatetrolleyfitness.ui.theme.UltimateTrolleyFitnessTheme
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
 
 class EmailPasswordActivity : ComponentActivity() {
-    private lateinit var auth: FirebaseAuth
-
-    private lateinit var editTextEmail: EditText
-    private lateinit var editTextPassword: EditText
-    private lateinit var editTextConfirmPassword: EditText
-    private lateinit var buttonLogin: Button
-    private lateinit var binding: ActivityEmailPasswordBinding
-    private val viewModel: EmailPasswordViewModel by viewModels()
-
-    fun isValidEmail(target: CharSequence?): Boolean {
-        return if (TextUtils.isEmpty(target)) {
-            false
-        } else {
-            Patterns.EMAIL_ADDRESS.matcher(target).matches()
-        }
+    private val viewModel: EmailPasswordViewModel by viewModels {
+        EmailPasswordViewModelFactory(this, application)
     }
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityEmailPasswordBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        viewModel.auth = FirebaseAuth.getInstance()
 
-        auth = FirebaseAuth.getInstance()
-        binding.viewModel = viewModel
-        binding.lifecycleOwner = this
-
-        editTextEmail = findViewById(R.id.editTextEmail)
-        editTextPassword = findViewById(R.id.editTextPassword)
-        buttonLogin = findViewById(R.id.buttonLogin)
-
-        binding.buttonLogin.setOnClickListener {
-            val email = binding.editTextEmail.text.toString()
-            val password = binding.editTextPassword.text.toString()
-
-            if (viewModel.isSignInState.value == true) {
-                signIn(email, password)
-            } else {
-                editTextConfirmPassword = findViewById(R.id.editTextConfirmPassword)
-                val confirmPassword = binding.editTextConfirmPassword.text.toString()
-                val emailValid = isValidEmail(email)
-
-                if(password == confirmPassword && emailValid) {
-                    createAccount(email, password)
-                } else {
-                    if(!emailValid) {
-                        Toast.makeText(
-                            baseContext,
-                            "Email is Invalid!",
-                            Toast.LENGTH_SHORT,
-                        ).show()
-                    }
-                    if(password !== confirmPassword) {
-                        Toast.makeText(
-                            baseContext,
-                            "Passwords don't match!",
-                            Toast.LENGTH_SHORT,
-                        ).show()
-                    }
-                }
+        setContent {
+            UltimateTrolleyFitnessTheme {
+                LoginScreen(viewModel)
             }
-        }
-
-        binding.buttonToggle.setOnClickListener {
-            viewModel.toggleState()
         }
     }
 
     public override fun onStart() {
         super.onStart()
 
-        val currentUser = auth.currentUser
+        val currentUser = viewModel.auth.currentUser
         if (currentUser != null) {
-            reload()
+            viewModel.reload()
         }
     }
 
-    private fun createAccount(email: String, password: String) {
-        auth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    Log.d(TAG, "createUserWithEmail:success")
-                    val user = auth.currentUser
-                    updateUI(user)
-                } else {
-                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                    Toast.makeText(
-                        baseContext,
-                        "Account creation failed.",
-                        Toast.LENGTH_SHORT,
-                    ).show()
-                }
-            }
-    }
-
-    private fun signIn(email: String, password: String) {
-        auth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    Log.d(TAG, "signInWithEmail:success")
-                    val user = auth.currentUser
-                    updateUI(user)
-                } else {
-                    Log.w(TAG, "signInWithEmail:failure", task.exception)
-                    Toast.makeText(
-                        baseContext,
-                        "Login failed.",
-                        Toast.LENGTH_SHORT,
-                    ).show()
-                }
-            }
-    }
-
-    private fun sendEmailVerification() {
-        val user = auth.currentUser!!
-        user.sendEmailVerification()
-            .addOnCompleteListener(this) { task ->
-                // Email Verification sent
-            }
-    }
-
-    private fun updateUI(user: FirebaseUser?) {
+    fun updateUI(user: FirebaseUser?) {
         // Launches main view, but should implement passing user object through to it.
-        val intent = Intent(this, MainActivity::class.java)
+        val intent = Intent(this@EmailPasswordActivity, MainActivity::class.java)
         startActivity(intent)
-    }
-
-    private fun reload() {
-
-    }
-
-    companion object {
-        private const val TAG = "EmailPassword"
     }
 }
 
 @Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun LoginScreen(viewModel: EmailPasswordViewModel) {
+    Column(
+        modifier = Modifier.padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        val isSignInState by viewModel.isSignInState.observeAsState(true)
+        val email: String by viewModel.email.observeAsState("")
+        val password: String by viewModel.password.observeAsState("")
+        val confirmPassword: String by viewModel.confirmPassword.observeAsState("")
+
+        EmailField(email, viewModel)
+        PasswordField(password, viewModel)
+
+        if (!isSignInState) {
+            ConfirmPasswordField(confirmPassword, viewModel)
+        }
+
+        Button(
+            onClick = {
+                if (isSignInState) {
+                    viewModel.signIn(email, password)
+                } else {
+                    viewModel.createAccount(email, password, confirmPassword)
+                }
+            }
+        ) {
+            Text(text = if (isSignInState) "Login" else "Create Account")
+        }
+
+        Button(
+            onClick = { viewModel.toggleState() }
+        ) {
+            Text(text = if (isSignInState) "Don't have an account? Create one!" else "Have an account? Login")
+        }
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun EmailField(email: String, viewModel: EmailPasswordViewModel) {
+    OutlinedTextField(
+        value = email,
+        onValueChange = { viewModel.onEmailChanged(it) },
+        singleLine = true,
+        label = { Text("Email Address") },
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = KeyboardType.Email
+        ),
+        textStyle = TextStyle.Default.copy(fontSize = 16.sp)
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview2() {
-    UltimateTrolleyFitnessTheme {
-        Greeting2("Android")
-    }
+@OptIn(ExperimentalMaterial3Api::class)
+fun PasswordField(password: String, viewModel: EmailPasswordViewModel) {
+    OutlinedTextField(
+        value = password,
+        onValueChange = { viewModel.onPasswordChanged(it) },
+        label = { Text("Password") },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = KeyboardType.Password
+        ),
+        textStyle = TextStyle.Default.copy(fontSize = 16.sp),
+        visualTransformation = PasswordVisualTransformation()
+    )
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun ConfirmPasswordField(confirmPassword: String, viewModel: EmailPasswordViewModel) {
+    OutlinedTextField(
+        value = confirmPassword,
+        onValueChange = { viewModel.onConfirmPasswordChanged(it) },
+        singleLine = true,
+        label = { Text("Confirm Password") },
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = KeyboardType.Password
+        ),
+        textStyle = TextStyle.Default.copy(fontSize = 16.sp),
+        visualTransformation = PasswordVisualTransformation()
+    )
 }
--- app/src/main/java/com/example/ultimatetrolleyfitness/EmailPasswordViewModel.kt
+++ app/src/main/java/com/example/ultimatetrolleyfitness/EmailPasswordViewModel.kt
@@ -1,14 +1,181 @@
 package com.example.ultimatetrolleyfitness
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.text.TextUtils
+import android.text.TextUtils.isEmpty
+import android.util.Log
+import android.util.Patterns
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.auth.FirebaseAuth
 
-class EmailPasswordViewModel : ViewModel() {
+class EmailPasswordViewModelFactory(
+    private val activity: EmailPasswordActivity,
+    private val application: Application
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(EmailPasswordViewModel::class.java)) {
+            return EmailPasswordViewModel(activity, application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
+
+
+class EmailPasswordViewModel(private val activity: EmailPasswordActivity, private val application: Application) : ViewModel() {
+    lateinit var auth: FirebaseAuth
+
     private val _isSignInState = MutableLiveData(true) // Default value: sign-in state
     val isSignInState: LiveData<Boolean> get() = _isSignInState
+
+    private var _email = MutableLiveData("")
+    var email: LiveData<String> = _email
+
+    private var _password = MutableLiveData("")
+    var password: LiveData<String> = _password
+
+    private var _confirmPassword = MutableLiveData("")
+    var confirmPassword: LiveData<String> = _confirmPassword
 
     fun toggleState() {
         _isSignInState.value = !_isSignInState.value!!
     }
+
+    fun onEmailChanged(newEmail: String) {
+        _email.value = newEmail
+    }
+
+    fun onPasswordChanged(newPassword: String) {
+        _password.value = newPassword
+    }
+
+    fun onConfirmPasswordChanged(newConfirmPassword: String) {
+        _confirmPassword.value = newConfirmPassword
+    }
+
+    fun createAccount(email: String, password: String, confirmPassword: String) {
+        val isEmailValid = isValidEmail(email)
+        val doesPasswordMatch = password == confirmPassword
+
+        if (isEmailValid
+            && doesPasswordMatch
+            && !isEmpty(email)
+            && !isEmpty(password)
+            && !isEmpty(confirmPassword)) {
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(activity) { task ->
+                    if (task.isSuccessful) {
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        activity.updateUI(user)
+                    } else {
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            activity.baseContext,
+                            "Account creation failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
+                }
+        } else {
+            if(!isEmpty(password) || !isEmpty(confirmPassword)) {
+                Toast.makeText(
+                    activity.baseContext,
+                    "Password fields must not be blank!",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            } else if (!doesPasswordMatch) {
+                Toast.makeText(
+                    activity.baseContext,
+                    "Passwords do not match!",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            }
+            if(isEmpty(email)) {
+                Toast.makeText(
+                    activity.baseContext,
+                    "Email must not be blank!",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            } else if(!isEmailValid) {
+                Toast.makeText(
+                    activity.baseContext,
+                    "Invalid email! Please try again.",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            }
+        }
+    }
+
+    fun signIn(email: String, password: String) {
+        val isEmailValid = isValidEmail(email)
+
+        if(isEmailValid && !isEmpty(email) && !isEmpty(password)){
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(activity) { task ->
+                    if (task.isSuccessful) {
+                        Log.d(TAG, "signInWithEmail:success")
+                        val user = auth.currentUser
+                        activity.updateUI(user)
+                    } else {
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            activity.baseContext,
+                            "Login failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
+                }
+        } else {
+            if(isEmpty(email)) {
+                Toast.makeText(
+                    activity.baseContext,
+                    "Email must not be blank!",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            } else if(!isEmailValid) {
+                Toast.makeText(
+                    activity.baseContext,
+                    "Invalid email! Please try again.",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            }
+
+            if(isEmpty(password)) {
+                Toast.makeText(
+                    activity.baseContext,
+                    "Password must not be blank!",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            }
+        }
+    }
+
+    fun sendEmailVerification() {
+        val user = auth.currentUser!!
+        user.sendEmailVerification()
+            .addOnCompleteListener(activity) { task ->
+                // Email Verification sent
+            }
+    }
+
+    fun reload() {
+
+    }
+
+    fun isValidEmail(target: CharSequence?): Boolean {
+        return if (TextUtils.isEmpty(target)) {
+            false
+        } else {
+            Patterns.EMAIL_ADDRESS.matcher(target).matches()
+        }
+    }
+
+    companion object {
+        private const val TAG = "EmailPassword"
+    }
+
 }
