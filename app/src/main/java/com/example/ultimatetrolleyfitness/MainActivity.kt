a49efca2948944d440d093a2381e8605055e642d
Soapery <darian.a.ronald@gmail.com>
2023-11-09 13:59:06 -0600

    Optimized imports and cleaned code.

--- app/src/main/java/com/example/ultimatetrolleyfitness/MainActivity.kt
+++ app/src/main/java/com/example/ultimatetrolleyfitness/MainActivity.kt
@@ -3,28 +3,13 @@ package com.example.ultimatetrolleyfitness
 import StepCounterHelper
 import android.os.Build
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import com.example.ultimatetrolleyfitness.ui.theme.UltimateTrolleyFitnessTheme
-import com.google.firebase.auth.FirebaseAuth
-import androidx.navigation.compose.composable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
 
@@ -54,7 +39,6 @@ class MainActivity : ComponentActivity() {
 fun ScaffoldWithBottomNav(navController: NavController) {
     BottomNavigationBar(navController)
 }
-
 
 
 //@Preview(showBackground = true)
